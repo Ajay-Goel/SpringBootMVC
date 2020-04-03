752ebf015ed637e80a271e4ba05a71cbf1f87321
Ajay-Goel <ajaygoel1993@gmail.com>
2020-04-03 02:26:35 -0400

    #25

---
+++ hplusapp/src/main/java/com/test/hplus/exceptions/ApplicationExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.test.hplus.exceptions;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.async.AsyncRequestTimeoutException;
+
+@ControllerAdvice
+public class ApplicationExceptionHandler {
+
+    @ExceptionHandler({ApplicationException.class, AsyncRequestTimeoutException.class})
+    public String handleException(){
+        System.out.println("in global handler");
+        return "error";
+    }
+
+}
